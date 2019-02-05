417e32e3c8e52eff9ba5e3afe40f84fa2dd62c62
Kim Oseland <koseland87@gmail.com>
2019-02-05 17:21:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,17 +10,3 @@ function logShout(string) {
 function logWhisper(string) {
   console.log('hello')
 }
-
-function sayHiToGrandma(string)', function() {
-  return ("I can't hear you!") if (string.toLowerCase) function() {
-    expect(sayHiToGrandma('hello')).toEqual("I can't hear you!")
-  })
-
-  it('returns "YES INDEED!" if `string` is uppercase', function() {
-    expect(sayHiToGrandma('HELLO')).toEqual("YES INDEED!")
-  })
-
-  it('returns "I love you, too." if `string` is "I love you, Grandma."`', function() {
-    expect(sayHiToGrandma("I love you, Grandma.")).toEqual("I love you, too.")
-  })
-})
